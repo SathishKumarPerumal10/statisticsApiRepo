d73ded388e27ede4913194dd7d3a607feed147e7
LMUK\sperumal <SatishKumar.Perumal@aimia.com>
2018-03-02 11:36:50 +0000

    Added more tests

--- src/main/java/com/n26/statistics/endpoint/rest/TransactionStatisticsRestController.java
+++ src/main/java/com/n26/statistics/endpoint/rest/TransactionStatisticsRestController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -31,8 +30,7 @@ public class TransactionStatisticsRestController {
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    @RequestMapping("/statistics")
-    @GetMapping
+    @RequestMapping(value = "/statistics", method = RequestMethod.GET)
     public TransactionStatisticsResponseDto getTransactionStatistics() {
         return transactionStatisticsService.getTransactionStatisticsForLastMin();
     }
---
+++ src/test/java/com/n26/statistics/service/endpoint/TransactionStatisticsRestControllerTest.java
@@ -0,0 +1,65 @@
+package com.n26.statistics.service.endpoint;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.n26.statistics.endpoint.rest.TransactionStatisticsRestController;
+import com.n26.statistics.exception.NoContentException;
+import com.n26.statistics.model.TransactionRequestDto;
+import com.n26.statistics.service.TransactionStatisticsService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.*;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+public class TransactionStatisticsRestControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private TransactionStatisticsService transactionStatisticsService;
+
+    @InjectMocks
+    private TransactionStatisticsRestController transactionStatisticsRestController;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(transactionStatisticsRestController).build();
+    }
+
+    @Test
+    public void whenCreateTransactionRequest_thenReturnResponseWithStatusCreated() throws Exception {
+
+        String requestBody = "{\"amount\":123.00,\"timestamp\":1519986840162}";
+        MvcResult result = mockMvc
+                .perform(MockMvcRequestBuilders.post("/transactions").content(requestBody).contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isCreated()).andReturn();
+    }
+
+    @Test
+    public void whenCreateTransactionRequestwithInvalidRequest_thenReturnResponseWithStatusNoContent() throws Exception {
+
+        doThrow(NoContentException.class).when(transactionStatisticsService).createTransactionStatistics(any(TransactionRequestDto.class));
+        String requestBody = "{\"amount\":123.00,\"timestamp\":1519986840162}";
+        MvcResult result = mockMvc
+                .perform(MockMvcRequestBuilders.post("/transactions").content(requestBody).contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isNoContent()).andReturn();
+    }
+
+    @Test
+    public void whenGetStatistics_thenReturnStatistics() throws Exception {
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/statistics").contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk()).andReturn();
+
+        assertNotNull(result);
+    }
+
+}
